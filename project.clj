d6f7a4c12949001c35e7760eebfc30e59e071c91
Punit Naik <naik.punit44@gmail.com>
2018-06-06 22:40:29 +0530

    updated libs

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject kubernetes-api "0.1.1-SNAPSHOT"
+(defproject kubernetes-api "0.1.1"
   :description "Kubernetes Client API Library"
   :url "https://github.com/yanatan16/clj-kubernetes-api"
   :license {:name "MIT"
             :url "https://github.com/yanatan16/clj-kubernetes-api/blob/master/LICENSE"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]
                  [org.clojure/data.json "0.2.6"]
-                 [http-kit "2.1.18"]]
+                 [http-kit "2.3.0"]]
 
   :codox {:namespaces [#"kubernetes\.api\.v\d.*"]}
 
